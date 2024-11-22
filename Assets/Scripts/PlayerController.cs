3dd1f3f656c344c9440d00c055390d2275c4e656
Quoc1610 <tranquoc58668@gmail.com>
2024-11-23 01:47:20 +0700

    Done Moving Cine
    - Floating joystick to move
    - Cinemachine to follow

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private FloatingJoystick joystick;
     [SerializeField] private float speed = 2f;
     [SerializeField] private float maxSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 50f; 
+    [SerializeField] private Animator animator;
     private Vector3 direction;
     // Start is called before the first frame update
     void Start()
@@ -20,13 +22,20 @@ public class PlayerController : MonoBehaviour
     {
         if (joystick.Direction != Vector2.zero)
         {
+            
             direction = Vector3.forward * joystick.Vertical + Vector3.right * joystick.Horizontal;
             direction.Normalize();
             controller.Move(direction * speed * Time.deltaTime);
             if(speed < maxSpeed)
             {
+
                 speed += 0.1f;
+                ChangeAnim(speed/maxSpeed);
             }
+            
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+       
         }
         else
         {
@@ -34,8 +43,14 @@ public class PlayerController : MonoBehaviour
             {
                 speed -= 0.1f;
                 controller.Move(direction * speed * Time.deltaTime);
+                ChangeAnim(speed/maxSpeed);
             }
 
         }
+        
+    }
+    private void ChangeAnim(float amount)
+    {
+        animator.SetFloat("Blend", amount);
     }
 }
